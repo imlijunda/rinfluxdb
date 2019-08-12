39c1dd88d06f91a43b72edf05e59596824c04ca3
Jun <imlijunda@gmail.com>
2019-08-13 03:37:41 +0930

    misc. doc update.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
 #' @import httr
 #' @importFrom lubridate is.Date is.POSIXct
-#' @importFrom stringr str_replace_all fixed
+#' @importFrom stringr str_replace_all fixed str_remove_all str_sub
 #' @importFrom stats runif
+#' @importFrom utils getFromNamespace
+#' @importFrom xts xtsAttributes
+#' @importFrom zoo index
 #'
 globals <- new.env()
 globals$dt_avail <- FALSE
